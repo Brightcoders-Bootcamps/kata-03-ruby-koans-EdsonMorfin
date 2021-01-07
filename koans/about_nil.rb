341b8c0952af08687b46c75c012d8ae4f2f047ef
EdsonPaul <edsonpaul2018@gmail.com>
2021-01-07 12:10:11 -0600

    about_nil error in test_you dont get_null... correction

--- koans/about_nil.rb
+++ koans/about_nil.rb
@@ -11,15 +11,16 @@ class AboutNil < Neo::Koan
     # What happens when you call a method that doesn't exist.  The
     # following begin/rescue/end code block captures the exception and
     # makes some assertions about it.
+    begin
+      nil.some_method_nil_doesnt_know_about
+    rescue Exception => ex
+      # What exception has been caught?
+      assert_equal NoMethodError, ex.class
 
-    nil.some_method_nil_doesnt_know_about
-  rescue Exception => example
-    # What exception has been caught?
-    assert_equal NoMethodError, example.class
-
-    # What message was attached to the exception?
-    # (HINT: replace __ with part of the error message.)
-    assert_match(/undefined method/, e.message)
+      # What message was attached to the exception?
+      # (HINT: replace __ with part of the error message.)
+      assert_match(/undefined method/, ex.message)
+    end
   end
 
   def test_nil_has_a_few_methods_defined_on_it
